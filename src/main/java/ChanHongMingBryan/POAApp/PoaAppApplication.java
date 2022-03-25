c1054af23c379c35a8d3f665c624193c4decc5c0
Bryan-CHM <chanhmbryan@gmail.com>
2022-03-25 09:18:07 +0800

    first commit

---
+++ src/main/java/ChanHongMingBryan/POAApp/PoaAppApplication.java
@@ -0,0 +1,13 @@
+package ChanHongMingBryan.POAApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ChanHongMingBryan/POAApp/PoaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ChanHongMingBryan.POAApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
